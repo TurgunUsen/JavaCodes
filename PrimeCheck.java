c584806a8a516e0a79f0c96d7bf1d3f4600bbe14
TurgunUsen <103962078+TurgunUsen@users.noreply.github.com>
2024-09-25 23:08:17 -0700

    Update PrimeCheck.java

--- PrimeCheck.java
+++ PrimeCheck.java
@@ -1,6 +1,6 @@
 public class PrimeCheck {
 
-    static boolean Prime(int number){
+    static boolean isPrime(int number){
         // prime number checker
         while (number % 2 == 0 || number <= 1){
             return false;
@@ -10,7 +10,7 @@ public class PrimeCheck {
 
     public static void main(String[] args) {
         int number = 14;
-        if (Prime(number)){
+        if (isPrime(number)){
             System.out.println(number + " is a prime number");
         } else {
             System.out.println(number + " is not a prime number");
