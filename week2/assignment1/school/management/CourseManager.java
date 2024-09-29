7bf2c76e3c42d8cd40c7960ec565396a3640d997
Tim U <ustitan@gmail.com>
2024-09-29 00:11:19 -0700

    Modified CourseManager.java

--- week2/assignment1/school/management/CourseManager.java
+++ week2/assignment1/school/management/CourseManager.java
@@ -3,14 +3,19 @@ import java.util.Arrays;
 
 public class CourseManager {
 
+    private Student[] students;
+    int studentCount = 0;
+
     // Constructor
     public CourseManager(){
         Student[] students = new Student[0];
-        int studentCount = 0;
     }
 
     // Adds students to the array
     public void addStudent(Student student){
+    students = Arrays.copyOf(students, studentCount++);
+    students[studentCount] = student;
+    studentCount++;
 
     }
 
