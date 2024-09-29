b2a0af644007a7672f5571b52e13f3bc7e4a41f5
Tim U <ustitan@gmail.com>
2024-09-28 23:03:05 -0700

    Created CourseManager.java

---
+++ week2/assignment1/school/management/CourseManager.java
@@ -0,0 +1,13 @@
+package week2.assignment1.school.management;
+import java.util.Arrays;
+
+public class CourseManager {
+
+    private Student[] students;
+
+
+    // Constructor
+    public CourseManager(){
+
+    }
+}
