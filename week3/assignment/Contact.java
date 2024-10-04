ee6b3882fcbb241fa08562abb703d8798a864751
Tim U <ustitan@gmail.com>
2024-10-03 18:37:56 -0700

    Modify Contact.java

--- week3/assignment/Contact.java
+++ week3/assignment/Contact.java
@@ -1,8 +1,16 @@
 package week3.assignment;
 
 public class Contact{
+
     private String name;
     private String phoneNumber;
     private String email;
+
+public Contact(String name, String phoneNumber, String email){
+    this.name = name;
+    this.phoneNumber = phoneNumber;
+    this.email = email;
+}
+
 
 }
