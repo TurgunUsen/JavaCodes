cdc9fca05886e834a8a94b58866cc6e47c090e01
Tim U <ustitan@gmail.com>
2024-10-03 21:24:36 -0700

    Upadate Contact.java

--- week3/assignment/Contact.java
+++ week3/assignment/Contact.java
@@ -6,10 +6,24 @@ public class Contact{
     private String phoneNumber;
     private String email;
 
+// Constructor
 public Contact(String name, String phoneNumber, String email){
     this.name = name;
     this.phoneNumber = phoneNumber;
     this.email = email;
+}
+
+// Getters
+public String getName(){
+    return name;
+}
+
+public String getPhoneNumber(){
+    return phoneNumber;
+}
+
+public String getEmail(){
+    return email;
 }
 
 
