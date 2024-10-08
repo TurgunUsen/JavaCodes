11b28ede81d28344cd2ecad5ec726787ecbc0ef7
Tim U <ustitan@gmail.com>
2024-10-07 17:08:12 -0700

    Updated Main.java

--- week3/assignment/Main.java
+++ week3/assignment/Main.java
@@ -1,5 +1,16 @@
 package week3.assignment;
+import java.util.*;
 
 public class Main{
 
+    public static void main(String[] args) {
+        ContactManager manager = new ContactManager();
+
+        // Example contacts
+        manager.addContact(new Contact("Alice", "1234567890", "alice@gmail.com"));
+        manager.addContact(new Contact("Bob", "9876543210", "bob@yahoo.com"));
+        manager.displayContacts();
+        manager.searchContact("Alice");
+        manager.extractEmailDomains();
+    }
 }
