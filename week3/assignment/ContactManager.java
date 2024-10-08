93ffd033ecd410b448a0ea3a605a1dfbe871404b
Tim U <ustitan@gmail.com>
2024-10-08 11:15:49 -0700

    Modify ContactManager.java

--- week3/assignment/ContactManager.java
+++ week3/assignment/ContactManager.java
@@ -1,10 +1,17 @@
 package week3.assignment;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 
 public class ContactManager {
-    ArrayList<Contact> contacts = new ArrayList<>();
+    // ArrayList for storing contacts
+    private ArrayList<Contact> contacts = new ArrayList<>();
+    // HashMap for phone number lookup by contact name
+    private HashMap<String, String> phoneBook = new HashMap<>();
+    // HashSet for storing unique email domains
+    private HashSet<String> emailDomains = new HashSet<>();
 
-    // Add a new contact to the list
+    // Add a new contact
     public void addContact(Contact contact) {
         // Check for duplicates based on name
         for (Contact c : contacts) {
@@ -13,7 +20,15 @@ public class ContactManager {
                 return;
             }
         }
+        // Add to ArrayList and HashMap (for name -> phone number)
         contacts.add(contact);
+        phoneBook.put(contact.getName(), contact.getPhoneNumber());
+
+        // Add the email domain to HashSet
+        String email = contact.getEmail();
+        String domain = email.substring(email.indexOf("@") + 1);
+        emailDomains.add(domain);
+
         System.out.println("Contact added: " + contact);
     }
 
@@ -28,4 +43,22 @@ public class ContactManager {
         }
     }
 
+    // Search for a contact by name and display their phone number
+    public void searchContact(String name) {
+        if (phoneBook.containsKey(name)) {
+            System.out.println("Phone number for " + name + ": " + phoneBook.get(name));
+        } else {
+            System.out.println("Contact not found.");
+        }
+    }
+
+    // Extract and display unique email domains from contacts
+    public void extractEmailDomains() {
+        if (emailDomains.isEmpty()) {
+            System.out.println("No email domains available.");
+        } else {
+            System.out.println("Unique email domains: " + emailDomains);
+        }
+    }
 }
+
