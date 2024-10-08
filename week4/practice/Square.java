3a71a165f6ef180c110e105632b56cb4449a1c4d
TurgunUsen <103962078+TurgunUsen@users.noreply.github.com>
2024-10-08 11:49:28 -0700

    Create Square.java

---
+++ week4/practice/Square.java
@@ -0,0 +1,15 @@
+public class Square extends Rectangle {
+    public Square(double side) {
+        super(side, side);
+    }
+
+    @Override
+    public String getShapeType() {
+        return "Square";
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Drawing a Square with side: " + super.calculateArea() / super.calculatePerimeter());
+    }
+}
