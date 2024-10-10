1c5d1eb5ced461207e97c99de6ac14a65a262ad5
Tim U <ustitan@gmail.com>
2024-10-09 22:39:52 -0700

    Move Main.java pracice2 folder

--- week3/practice2/Main.java
+++ week3/practice2/Main.java
@@ -1,4 +1,4 @@
-package week3.practice1;
+package week3.practice2;
 import java.util.HashMap;
 
 public class Main {
@@ -6,8 +6,11 @@ public class Main {
     public static int[] twoSum(int[] nums, int target) {
 
         HashMap<Integer, Integer> complements = new HashMap<>();
+
         for(int i=0;i<nums.length;i++) {
+
             int val = nums[i];
+
             // target == val + complement
             int complement = target-val;
             System.out.println("val: " + val + " index: " + i + " complement: "+ complement);
@@ -20,6 +23,7 @@ public class Main {
     }
 
     public static void main(String[] args) {
+
         //            0,1,2,3
         int[] nums = {5,8,3,9};
         int t  =13;
