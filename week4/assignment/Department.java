6246b5978dbb118b57ef6b3d6018fd5819996329
TurgunUsen <103962078+TurgunUsen@users.noreply.github.com>
2024-10-13 16:59:03 -0700

    Create Department.java

---
+++ week4/assignment/Department.java
@@ -0,0 +1,9 @@
+package week4.assignment;
+import java.util.ArrayList;
+
+public class Department{
+  
+    private String name;
+    private ArrayList<Professor> Professors;
+    private ArrayList<Course> Courses;
+}
