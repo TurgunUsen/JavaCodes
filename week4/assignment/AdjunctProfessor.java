c64ed0629a3249a33ed6f52a7c6818fc1db5facd
Tim U <ustitan@gmail.com>
2024-10-14 16:07:41 -0700

    Update AdjunctProfessor.java

--- week4/assignment/AdjunctProfessor.java
+++ week4/assignment/AdjunctProfessor.java
@@ -1,4 +1,21 @@
+package week4.assignment;
+
 public class AdjunctProfessor extends Professor {
     private int coursesTaught;
 
+public AdjunctProfessor(String name, String department, int coursesTaught) {
+    super(name, department);
+    this.coursesTaught = coursesTaught;
+    }
+// Override teachCourse
+@Override
+public void teachCourse(){
+    system.out.println("Adjunct Professor " + getName() + " is teaching." );
+    }
+
+// Overide calculateSalary
+@Override
+public double calculateSalary(){
+    return coursesTaught * 2000;
+}
 }
