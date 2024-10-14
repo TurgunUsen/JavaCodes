08abfd246efdd07e294e98d627e1a114b040dd16
TurgunUsen <103962078+TurgunUsen@users.noreply.github.com>
2024-10-13 20:02:51 -0700

    Update Department.java

--- week4/assignment/Department.java
+++ week4/assignment/Department.java
@@ -6,4 +6,35 @@ public class Department{
     private String name;
     private ArrayList<Professor> Professors;
     private ArrayList<Course> Courses;
+
+  // Constructor
+ public Department(String name) {
+    this.name = name;
+    this.professors = new ArrayList<>();
+    this.courses = new ArrayList<>();
+    }
+
+  // Add Professor
+  public void addProfessor(Professor professor){
+    this.professors.add(professor);
+  }
+
+  // Add Course
+  public void addCourse(Course course){
+    this.courses.add(course);
+  }
+
+  // Getters
+  public ArrayList<Professor> getProfessors(){
+    return professors;
+  }
+
+  public ArrayList<Course> getCourses(){
+    return courses
+  }
+
+  // Display department details
+  public void displayDepartmentDetails(){
+    
+  }
 }
