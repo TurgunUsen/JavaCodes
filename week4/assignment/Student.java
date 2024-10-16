7a53211f2c61d295daa46637eff135f44225b1a6
Tim U <ustitan@gmail.com>
2024-10-15 21:02:50 -0700

    Create Student java file

---
+++ week4/assignment/Student.java
@@ -0,0 +1,15 @@
+package week4.assignment;
+import java.util.HashMap;
+
+public class Student {
+    private String name;
+    private String studentID;
+    private HashMap<String, Integer> grades;
+
+    //Constructor
+    public Student(String name, String studentID){
+        this.name = name;
+        this.studentID = studentID;
+        this.grades = new HashMap<>();
+    }
+}
