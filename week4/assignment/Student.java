4de1204d4cc2f0d51edcc061abd314ec23557cea
Turgun <ustitan@gmail.com>
2024-10-16 15:56:32 -0700

    Update Student.java

--- week4/assignment/Student.java
+++ week4/assignment/Student.java
@@ -5,11 +5,22 @@ public class Student {
     private String name;
     private String studentID;
     private HashMap<String, Integer> grades;
-}
+
     // Constructor
     public Student(String name, String studentID){
         this.name = name;
         this.studentID = studentID;
         this.grades = new HashMap<>();
+    }
+    // Register for a course
+    public void registerForCourse(Course course){
+        grades.put(courseName, grade);
+    }
+    //Get grade report
+    public void displayGradeReport(){
+        System.out.println("Grade report for " + name + ":");
+        for (String course : grades.keySet()){
+            System.out.println(course + ": " + grades.get(course));
+        }
     }
 }
