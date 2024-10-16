9d834b3fdef7616f162512c4fd61d40f593a9e40
TurgunUsen <103962078+TurgunUsen@users.noreply.github.com>
2024-10-16 15:16:00 -0700

    Update Student.java

--- week4/assignment/Student.java
+++ week4/assignment/Student.java
@@ -1,28 +1,15 @@
-<<<<<<< HEAD
 package week4.assignment;
-=======
-package week4.assignmnet;
->>>>>>> 1bad3137dcb5ae41c825caedfa3bf7a088d62b9b
 import java.util.HashMap;
 
 public class Student {
     private String name;
     private String studentID;
     private HashMap<String, Integer> grades;
-<<<<<<< HEAD
-
-    //Constructor
-=======
-    
+}
     // Constructor
->>>>>>> 1bad3137dcb5ae41c825caedfa3bf7a088d62b9b
     public Student(String name, String studentID){
         this.name = name;
         this.studentID = studentID;
         this.grades = new HashMap<>();
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 1bad3137dcb5ae41c825caedfa3bf7a088d62b9b
