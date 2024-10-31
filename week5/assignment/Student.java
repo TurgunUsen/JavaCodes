4e2c5f672975dc4b4c65595247f9df65f3b12edf
TurgunUsen <103962078+TurgunUsen@users.noreply.github.com>
2024-10-31 16:59:20 -0700

    Create Student.java

---
+++ week5/assignment/Student.java
@@ -0,0 +1,25 @@
+package week5.assignment;
+
+class Student {
+    private String name;
+    private int age;
+
+    public Student(String name, int age) throws InvalidAgeException, InvalidNameException {
+        if (name == null || name.isEmpty()) {
+            throw new InvalidNameException("Name cannot be null or empty.");
+        }
+        if (age < 18 || age > 60) {
+            throw new InvalidAgeException("Age must be between 18 and 60.");
+        }
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
